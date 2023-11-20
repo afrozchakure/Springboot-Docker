2bf198ba870b8595e9c5d512f1c88a5b1a7f540b
Afroz Chakure <aaaanchakure@gmail.com>
2023-11-20 14:38:45 +0530

    this is updated Test case

--- src/test/java/com/beginsecure/hw/HwApplicationTests.java
+++ src/test/java/com/beginsecure/hw/HwApplicationTests.java
@@ -1,13 +1,24 @@
 package com.beginsecure.hw;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class HwApplicationTests {
+@AutoConfigureMockMvc
+public class HwApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	public void testHwServiceStatus() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/hw"))
+				.andExpect(status().isOk());
 	}
-
 }
